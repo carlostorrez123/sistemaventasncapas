b7b8cc1bd02a69673753ab6d9547ef0f07cd09db
LABORATORIO A <achacurva70@gmail.com>
2024-03-13 10:19:28 -0400

    actualizacion

---
+++ SistemaVentas/SistemasVentas.BSs/PersonaBss.cs
@@ -0,0 +1,20 @@
+﻿using SistemasVentas.DAL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemasVentas.BSs
+{
+    public class PersonaBss
+    {
+        PersonaDal dal = new PersonaDal();
+        public DataTable ListarPersonaBss()
+        {
+            return dal.ListarPersonaDal();
+        }
+    }
+}
